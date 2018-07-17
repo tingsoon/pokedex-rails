d744f5c1864b7fae0ff7d1541e80036929b0ce1e
tingsoon <angts8@hotmail.com>
2018-07-17 22:23:11 +0800

    completed hw

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,43 @@
+class PokemonController < ApplicationController
+    def index
+      @pokemons = Pokemon.all
+    end
+  
+    def show
+      @pokemon = Pokemon.find(params[:id])
+    end
+  
+    def new
+    end
+  
+    def edit
+      @pokemon = Pokemon.find(params[:id])
+    end
+  
+    def create
+      @pokemon = Pokemon.new(pokemon_params)
+
+      @pokemon.save
+      redirect_to @pokemon
+    end
+  
+    def update
+      @Pokemon = Pokemon.find(params[:id])
+
+      @pokemon.update(pokemon_params)
+      redirect_to @pokemon
+    end
+  
+    def destroy
+      @pokemon = Pokemon.find(params[:id])
+      @pokemin.destroy
+    
+      redirect_to pokemons_path
+    end
+
+    private
+      def pokemon_params
+        params.require(:pokemon).permit(:id, :num, :name, :img, :height, :weight)
+      end
+    
+end
